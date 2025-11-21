eb7da8ee52f0fefe65382fc09ff07a3b2dbe4d84
Olivia Han <72257046+Olive2026@users.noreply.github.com>
2025-11-21 22:12:15 +0000

    Created Lot Class

---
+++ Lot1.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Lot1 
+{
+    public int lotStatus;
+    public final int lotNumber;
+
+    public Lot1(int lotNum, int status) 
+    {
+        lotNumber = lotNum;
+        lotStatus = status;
+        System.out.println("new lot: " + lotNum + " status: " + status);
+    }
+    public void setStatus(int newStatus)
+    {
+        this.lotStatus = newStatus; 
+    }
+    public int getLotStatus()
+    {
+        return lotStatus;
+    }
+    public int getLotNum()
+    {
+        return lotNumber;
+    }
+}
