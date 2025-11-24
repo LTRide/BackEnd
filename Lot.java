0c60747ae167ef430cd19499c8915643f93512dd
Olivia <oliviahan2308@gmail.com>
2025-11-24 09:30:57 -0600

    Error-filled full Lot class

--- Lot.java
+++ Lot.java
@@ -1,4 +1,3 @@
-import java.io.*;
 import java.util.*;
 
 public class Lot 
@@ -6,36 +5,97 @@ public class Lot
     private final String lotName;
     private Map<Integer, Boolean> spaces = new HashMap<>();
     private Map<String, Integer> storeSpaceId = new HashMap<>();
-    private final Double timeStamp = null;
-    private final Integer capacity = null; 
+    private Double timeStamp;
+    private Integer capacity; 
 
-    public Lot(String lotName) 
+    public Lot(String lotName) //Maybe change?
     {
        this.lotName = lotName;
     }
+    // Access the Lot name
     public String getLotName()
     {
         return this.lotName;
     }
-    // Map the sensor to the space
-    public void setSpaceId(String sensorId, Integer spaceId)
+    // Access time stamp of parking lot accesability update
+    public Double getTimeStamp()
     {
-        this.storeSpaceId.put(sensorId, spaceId);
+        return timeStamp;
+    }
+    // Access the current lot capacity
+    public Integer getLotCapacity()
+    {
+        return capacity;
+    }
+    // Update time stamp
+    public void setTimeStamp(Double time)
+    {
+        this.timeStamp = time;
+    }
+    // Update capacity of lot
+    public void setLotCapacity(Integer capacity)
+    {
+        this.capacity = capacity;
+    }
+
+    // Map the sensor to the space
+    public void setSpaceName(String sensorId, Integer spaceName)
+    {
+        this.storeSpaceId.put(sensorId, spaceName);
     }
     //Access the mapped space id
-    public Integer getSpaceId(String sensorId)
+    public Integer getSpaceName(String sensorId)
     {
         return this.storeSpaceId.get(sensorId);
     }
     // Update space status
     public void setSpaceStatus(Boolean status, String sensorId)
     {
-        Integer spaceId = getSpaceId(sensorId);
-        this.spaces.put(spaceId, status);
+        Integer spaceName = getSpaceName(sensorId);
+        this.spaces.put(spaceName, status);
     }
     //Access the space status
-    public Boolean getSpaceStatus(Integer spaceId)
+    public Boolean getSpaceStatus(Integer spaceName)
     {
-        return this.spaces.get(spaceId);
+        return this.spaces.get(spaceName);
+    }
+
+    public static void main(String [] args)
+    {
+        System.out.println("Hello World");
+         
+        Lot adminLot = new Lot("admin lot");
+        
+        String lotName = adminLot.getLotName();
+        System.out.println("Lot name: " + lotName);
+
+        String sensorId = "N123";
+        adminLot.setSpaceName(sensorId, 1);
+        
+        Integer spaceName = adminLot.getSpaceName(lotName);
+        adminLot.setSpaceStatus(false, sensorId);
+        Boolean spaceStatus = adminLot.getSpaceStatus(spaceName); // Might change ref system
+
+        String status;
+        if(spaceStatus)
+        {
+            status = "full";
+        }
+        else
+        {
+            status = "available";
+        }
+        System.out.println("Lot " + spaceName + " is " + status);
+        
+        adminLot.setLotCapacity(14);
+        Integer lotCapacity = adminLot.getLotCapacity();
+        System.out.println("Lot capacity: " + lotCapacity);
+
+        adminLot.setTimeStamp(12.45);
+        Double lotTimeStamp = adminLot.getTimeStamp();
+        System.out.println("The lot was updated " + lotTimeStamp + " minutes ago");
+
+
+
     }
 }
--- Main.java
+++ Main.java
@@ -1,16 +1,40 @@
-import java.io.*;
-import java.util.*;
-
 
 public class Main
 {
      public static void main(String [] args)
     {
-        Lot newLot = new Lot(4, 1);
-        newLot.setStatus(0);
-        int lotNum = newLot.getLotNum();
-        int status = newLot.getLotStatus();
+        System.out.print("Hello World");
+         
+        Lot adminLot = new Lot("admin lot");
+        
+        String lotName = adminLot.getLotName();
+        System.out.println("Lot name: " + lotName);
+        
+        adminLot.setLotCapacity(14);
+        Integer lotCapacity = adminLot.getLotCapacity();
+        System.out.println("Lot capacity: " + lotCapacity);
 
-        System.out.println("lot " + lotNum + " has status: " + status);
+        adminLot.setTimeStamp(12.45);
+        Double lotTimeStamp = adminLot.getTimeStamp();
+        System.out.println("The lot was updated " + lotTimeStamp + " minutes ago");
+
+        String sensorId = "N123";
+        adminLot.setSpaceName(sensorId, 1);
+        Integer spaceName = adminLot.getSpaceName(lotName);
+        adminLot.setSpaceStatus(false, sensorId);
+        Boolean spaceStatus = adminLot.getSpaceStatus(spaceName); // Might change ref system
+
+        String status;
+        if(spaceStatus)
+        {
+            status = "full";
+        }
+        else
+        {
+            status = "available";
+        }
+        System.out.println("Lot " + spaceName + " is " + status);
+
+
     }
 }
